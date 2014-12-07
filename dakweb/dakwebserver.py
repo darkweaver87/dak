0d67a8de82f39c6ab09a878126074ab71555dcb3
Joerg Jaspert <joerg@debian.org>
2014-12-07 20:37:11 +0100

    .
    Signed-off-by: Joerg Jaspert <joerg@debian.org>

--- dakweb/dakwebserver.py
+++ dakweb/dakwebserver.py
@@ -28,7 +28,7 @@ def list_paths():
     """Returns a list of available paths"""
     bottle.response.content_type = 'text/plain'
     for path in QueryRegister().get_paths():
-        yield "https://ftp-master.debian.org/epydoc/%s-module.html#%s" % (QueryRegister().get_path_help(path), path))
+        yield "https://ftp-master.debian.org/epydoc/%s-module.html#%s" % (QueryRegister().get_path_help(path), path)
         yield "\n"
 #    return json.dumps(QueryRegister().get_paths())
 QueryRegister().register_path('/list_paths', list_paths)
